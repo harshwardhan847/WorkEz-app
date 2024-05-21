5954395dcaac35e44db5b53c6e6ee89ab7335d48
Harsh Wardhan <harshwardhan847@gmail.com>
2024-05-21 13:58:25 +0530

    minor

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class FullScreenWebView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: SafeArea(
         child: Column(
           children: [
